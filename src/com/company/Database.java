fb55621c40bf0e93474a291c74b266a9e744e7a6
lexis2450 <lexis2450@gmail.com>
2018-09-20 23:01:13 +0400

    Customer and Product are created

--- src/com/company/Database.java
+++ src/com/company/Database.java
@@ -1,7 +1,9 @@
 package com.company;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Database implements Serializable{
-
+    private List<String> arrCustomer = new ArrayList<String>();
+    private List<String> arrProduct = new ArrayList<String>();
 }
